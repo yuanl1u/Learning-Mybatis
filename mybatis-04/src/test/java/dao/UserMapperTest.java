776e5c28e67a6076d1c90e3fddcf3dc2ecf01c4b
yuanl1u <yuanliu19961209@gmail.com>
2021-06-22 15:50:40 +0800

    log4j

--- mybatis-04/src/test/java/dao/UserMapperTest.java
+++ mybatis-04/src/test/java/dao/UserMapperTest.java
@@ -4,6 +4,7 @@ import com.yuan.dao.UserMapper;
 import com.yuan.pojo.User;
 import com.yuan.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 /**
@@ -12,8 +13,13 @@ import org.junit.Test;
  */
 public class UserMapperTest {
 
+    static Logger logger = Logger.getLogger(UserMapperTest.class);
+
     @Test
     public void testSelectUserList(){
+        // 输出到日志 一眼就可以看见相对位置
+        // 代替sout输出 而且有多个等级 info, debug, error，且可以在日志中永久保存
+        logger.info("进入testSelectUserList成功!!");
         // 第一步：获取SqlSession对象
         SqlSession sqlSession = MybatisUtils.getSqlSession();
         // 第二步：执行(方式一getMapper)
@@ -24,5 +30,12 @@ public class UserMapperTest {
 
         // 关闭SqlSession
         sqlSession.close();
+    }
+
+    @Test
+    public void testLog4j(){
+        logger.info("info: 进入了testLog4j");
+        logger.debug("debug: 进入了testLog4j");
+        logger.error("error: 进入了testLog4j");
     }
 }
